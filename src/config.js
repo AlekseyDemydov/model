2498a71428775e6c731871789e8e204e72050570
Олексій Демидов <agdemidof@gmail.com>
2024-07-24 10:25:02 +0300

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: 'https://modelingg-c6b66dfa04bd.herokuapp.com'
+  baseURL: 'https://queensmodel.website'
     // baseURL: 'http://localhost:4444'
     // baseURL:process.env.REACT_APP_API_URL,
   };
